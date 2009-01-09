a574ed367a5a4c799fa522a211b786a97c8e910d
Piotr Usewicz <piotr@layer22.com>
2009-01-09 17:18:02 +0000

    Add README and make code nicer ;]

---
+++ README.md
@@ -0,0 +1,7 @@
+Sequel for Rails
+================
+
+Installation
+------------
+
+    script/plugin install git://github.com/pusewicz/rails_sequel.git
--- lib/rails_sequel.rb
+++ lib/rails_sequel.rb
@@ -1,19 +1,23 @@
 module Rails
   module SequelConnection
-    @@config = YAML::load(ERB.new(IO.read(Rails.root + "/config/database.yml")).result)[Rails.env].with_indifferent_access
+    CONFIG = YAML::load(ERB.new(IO.read(Rails.root + "/config/database.yml")).result)[Rails.env].with_indifferent_access
     
     def self.connect
       Sequel.connect uri
     end
     
+    def self.config
+      CONFIG
+    end
+    
     def self.uri
-      uri = @@config[:adapter] << "://"
-      uri << @@config[:username] if @@config[:username]
-      uri << ':' << @@config[:password] if @@config[:password]
-      uri << '@' if @@config[:username] || @@config[:password]
-      uri << ':' << @@config[:port] if @@config[:port]
-      uri << @@config[:host]
-      uri << '/' << @@config[:database]
+      uri = config[:adapter] << "://"
+      uri << config[:username] if config[:username]
+      uri << ':' << config[:password] if config[:password]
+      uri << '@' if config[:username] || config[:password]
+      uri << ':' << config[:port] if config[:port]
+      uri << config[:host]
+      uri << '/' << config[:database]
     end
   end
 end
