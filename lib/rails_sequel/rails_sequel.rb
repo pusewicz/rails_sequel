52377c1375c8ec242b799d0006cb19e745487327
Paweł Kondzior <kondzior.p@gmail.com>
2009-01-12 22:24:16 +0800

    Loggers should be array]
    Signed-off-by: Piotr Usewicz <piotr@layer22.com>

--- lib/rails_sequel/rails_sequel.rb
+++ lib/rails_sequel/rails_sequel.rb
@@ -32,7 +32,7 @@ module Rails
       options[:database] = config[:database] || "hey_dude_configure_your_database"
       # MSSQL support
       options[:db_type] = config[:db_type] if config[:db_type]
-      options[:loggers] = Rails.logger
+      options[:loggers] = [Rails.logger]
       options
     end
   end
