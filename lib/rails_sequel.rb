91612c8d5ea215aa8c2ab9a5869ddb3e680a41ec
Piotr Usewicz <piotr@layer22.com>
2009-01-14 23:54:48 +0000

    Remove expanded paths

--- lib/rails_sequel.rb
+++ lib/rails_sequel.rb
@@ -1,8 +1,8 @@
 # gem 'sequel'
 require 'sequel'
 
-require File.expand_path(File.dirname(__FILE__) + '/rails_sequel/rails_sequel')
-require File.expand_path(File.dirname(__FILE__) + '/rails_sequel/version')
-require File.expand_path(File.dirname(__FILE__) + '/rails_sequel/sequel_ext')
+require 'rails_sequel/rails_sequel'
+require 'rails_sequel/version'
+require 'rails_sequel/sequel_ext'
 
 Rails::SequelConnection.connect
